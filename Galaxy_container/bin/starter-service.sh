578610efd9a73399a1d1c8651e6651d745d45395
bedro <bedro@psb.ugent.be>
2019-04-03 17:46:59 +0200

    Fixed workflow loading download error

--- Galaxy_container/bin/starter-service.sh
+++ Galaxy_container/bin/starter-service.sh
@@ -8,5 +8,5 @@ startup &
 galaxy_instance="http://localhost:8080"
 galaxy-wait -g $galaxy_instance
 
-
+echo "Loading workflow" 
 python "/startingWorkflow.py" $1 
--- Galaxy_container/bin/startingWorkflow.py
+++ Galaxy_container/bin/startingWorkflow.py
@@ -9,59 +9,61 @@ GALAXY_URL = 'http://localhost:80'
 library_name = 'Local data'
 history_name = 'Hystory'
 output_history_name = 'Output hystory'
-dataset_name_1 = 'UCSC_input.bed'
+dataset_namings = ['UCSC_input.bed']
 
-#- Create Galaxy Instance Object
+# - Create Galaxy Instance Object
 gi = GalaxyInstance(
     GALAXY_URL, email='admin@galaxy.org', password='admin')
 
-#- Create history
+# - Create history
 gi.histories.create_history(name=history_name)
 histories = gi.histories.get_histories(name=history_name)
 history_id = histories[0]['id']
 print('History ID: ' + history_id)
 
-#- Create Libary
-#Default data is lodad from mounted directory
+# - Create Libary
+# Default data is lodad from mounted directory
 if sys.argv[1] == 'local':
     gi.libraries.create_library(name=library_name)
     libraries = gi.libraries.get_libraries(name=library_name)
     library_id = libraries[0]['id']
-#when env variable datalibr is given, datalibrary already made with setup-data-libraries.
+# when env variable datalibr is given, datalibrary already made with setup-data-libraries.
 elif sys.argv[1] == 'datalibr':
     libraries = gi.libraries.get_libraries()
     library_id = libraries[0]['id']
 
 print('Library ID: ' + library_id)
 
-#- Upload mounted input data in library
+# - Upload mounted input data in library
 gi.libraries.upload_file_from_server(library_id, './inputData')
 
-#- Load data in history
+# - Load data in history
 files = gi.libraries.show_library(library_id, contents=True)
 for f in files:
     if f['type'] == 'file':
-        dataset = gi.histories.upload_dataset_from_library(history_id, f['id'])
+        gi.histories.upload_dataset_from_library(history_id, f['id'])
 
-#- Check for installed workflow
+# - Check for installed workflow
 workflows = gi.workflows.get_workflows()
 workflow_id = workflows[0]['id']
 print('Workflow ID: ' + library_id)
 
-#- Examine workflow
+# - Examine workflow
 wf = gi.workflows.show_workflow(workflow_id)
 print('Inputs workflow:' + str(wf['inputs']))
 
 
-#- Determining input data
-dataset_1 = gi.histories.show_matching_datasets(
-    history_id, name_filter=dataset_name_1)
-print('Input dataset 1: ' + dataset_1[0]['name'])
+# - Determining input data
 datamap = dict()
-datamap['0'] = { 'src':'hda', 'id':dataset_1[0]['id'] } 
+for i, dataset_name in enumerate(dataset_namings):
+    dataset = gi.histories.show_matching_datasets(
+        history_id, name_filter=dataset_name)
+    print('Input dataset {}: '.format(str(i)) + dataset[0]['name'])
+    datamap[str(i)] = {'src': 'hda', 'id': dataset[0]['id']}
 
-#- Running workflow
-gi.workflows.invoke_workflow(workflow_id, inputs=datamap, history_name=output_history_name)
+# - Running workflow
+gi.workflows.invoke_workflow(
+    workflow_id, inputs=datamap, history_name=output_history_name)
 print('Workflow invoked')
 time.sleep(2)
 output_histories = gi.histories.get_histories(name=output_history_name)
@@ -71,9 +73,11 @@ while gi.histories.get_status(output_history_id)['percent_complete'] != 100:
     time.sleep(1)
 print('Workflow Done.')
 
-#- Export output files
-output_files = gi.histories.show_history(output_history_id, contents=True,  visible=True)
+# - Export output files
+output_files = gi.histories.show_history(
+    output_history_id, contents=True,  visible=True)
 for of in output_files:
     if of['history_content_type'] == 'dataset':
         print('Exporting ' + of['name'])
-        gi.datasets.download_dataset(of['id'], file_path='./mountDir/outputData')
+        gi.datasets.download_dataset(
+            of['id'], file_path='/mountDir/outputData')
