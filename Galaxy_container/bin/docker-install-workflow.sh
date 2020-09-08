c7a101451a4ec4c0770026238de00d92ae855367
bedroesb <bedro@psb.vib-ugent.be>
2020-09-08 10:30:51 +0200

    update

--- Galaxy_container/bin/docker-install-workflow.sh
+++ Galaxy_container/bin/docker-install-workflow.sh
@@ -1,38 +1,86 @@
 #!/bin/bash
 
-# install everything for workflow to work inside container
+# set API key
+API_KEY=${GALAXY_DEFAULT_ADMIN_KEY:-fakekey}
 
-set -e
-galaxy_instance="http://localhost:8080"
+# Enable Test Tool Shed
+echo ".. Setting up conda"
+export GALAXY_CONFIG_TOOL_SHEDS_CONFIG_FILE=$GALAXY_HOME/tool_sheds_conf.xml
 
-# launch the instance
-echo "Starting Galaxy..."
+. /tool_deps/_conda/etc/profile.d/conda.sh
+conda activate base
 
-export GALAXY_CONFIG_TOOL_PATH=/galaxy-central/tools/
-startup_lite
+if pgrep "supervisord" > /dev/null
+then
+    echo ".. System is up and running. Starting with the installation."
+    export PORT=80
+else
+    # start Database
+    echo ".. Starting database"
+    export PORT=8080
+    service postgresql start
+    install_log='galaxy_install.log'
 
-# wait until galaxy has started
-galaxy-wait -g $galaxy_instance
+    # wait for database to finish starting up
+    STATUS=$(psql 2>&1)
+    while [[ ${STATUS} =~ "starting up" ]]
+    do
+      echo ".. Waiting for database: $STATUS"
+      STATUS=$(psql 2>&1)
+      sleep 1
+    done
+    # start Galaxy
+    echo ".. Starting Galaxy"
+    # Unset SUDO_* vars otherwise conda run chown based on that
+    sudo -E -u galaxy -- bash -c "unset SUDO_UID; \
+        unset SUDO_GID; \
+        unset SUDO_COMMAND; \
+        unset SUDO_USER; \
+        ./run.sh -d $install_log --pidfile galaxy_install.pid --http-timeout 3000"
 
-# run install as user galaxy
-su - $GALAXY_USER
+    galaxy_install_pid=`cat galaxy_install.pid`
+    galaxy-wait -g http://localhost:$PORT -v --timeout 120
+    echo ".. Galaxy is running"
+fi
+
+# Create the admin user if not already done
+# Starting with 20.05 this user is only created at first startup of galaxy
+# We need to create it here for Galaxy Flavors = installing from Dockerfile
+if [[ ! -z $GALAXY_DEFAULT_ADMIN_USER ]]
+    then
+        (
+        cd $GALAXY_ROOT
+        . $GALAXY_VIRTUAL_ENV/bin/activate
+        echo ".. Creating admin user $GALAXY_DEFAULT_ADMIN_USER with key $GALAXY_DEFAULT_ADMIN_KEY and password $GALAXY_DEFAULT_ADMIN_PASSWORD if not existing"
+        python /usr/local/bin/create_galaxy_user.py --user "$GALAXY_DEFAULT_ADMIN_EMAIL" --password "$GALAXY_DEFAULT_ADMIN_PASSWORD" \
+        -c "$GALAXY_CONFIG_FILE" --username "$GALAXY_DEFAULT_ADMIN_USER" --key "$GALAXY_DEFAULT_ADMIN_KEY"
+        )
+fi
 
 # install workflow
 echo " - Extracting tools from workflows"
 for w in /workflowDir/*.ga
 do
     echo $w
-    workflow-to-tools -w $w -o /workflowDir/wftools.yaml -l "Tools from workflows"
-    echo " - Installing tools from workflow" 
-    shed-tools install -t /workflowDir/wftools.yaml -g $galaxy_instance -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD
-    
+    workflow-to-tools -w $w -o $tutdir/workflows/wftools.yaml -l $tut
+    echo " - Installing tools from workflow $(basename $w)" 
+    n=0
+    until [ $n -ge 3 ]
+    do
+        shed-tools install -t $tutdir/workflows/wftools.yaml -g "http://localhost:$PORT" -a $API_KEY && break
+        n=$[$n+1]
+        sleep 5
+        echo " - Retrying shed-tools install "
+    done        
+    rm $tutdir/workflows/wftools.yaml          
 done
 echo " - Installing workflows"
-    workflow-install --publish_workflows --workflow_path /workflowDir/ -g $galaxy_instance -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD
+workflow-install --publish_workflows --workflow_path $tutdir/workflows/ -g "http://localhost:$PORT" -a $API_KEY
 
 echo "Finished installation of workflow"
 
 
+echo "-------------------------------------------------------------"
 # check if data-library_all.yaml is not empty
 echo "Checking for data libraries"
 file="/workflowDir/data-library.yaml"
@@ -40,9 +88,30 @@ if [ -f "$file" ];
 then    
     if [ "$(head -n 1 $file)" != "{}" ];
     then
-        setup-data-libraries -i $file -g $galaxy_instance -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD -v
+        setup-data-libraries -i $file -g $galaxy_instance -a $API_KEY -v
         echo "Data library installed"
     else
         echo "No data libraries to install"
     fi
 fi
+
+exit_code=$?
+
+if [ $exit_code != 0 ] ; then
+    if [ "$2" == "-v" ] ; then
+        echo ".. Installation failed, Galaxy server log:"
+        cat $install_log
+    fi
+    exit $exit_code
+fi
+
+if ! pgrep "supervisord" > /dev/null
+then
+    echo ".. Shutting down Galaxy and postgresql"
+    # stop everything
+    sudo -E -u galaxy /galaxy-central/run.sh --stop --pidfile /galaxy-central/galaxy_install.pid
+    rm /galaxy-central/$install_log
+    service postgresql stop
+fi
+
+echo ".. Installation is finished"
